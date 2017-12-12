5181880e869ac3569108e4fbc13263fedcb1e3ba
dungeonMaster97 <raghavbhasin97@gmail.com>
2017-12-12 17:13:55 -0500

    Unity Add

---
+++ Unity/Assets/SolarSystem/scripts/RemoteCommand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[System.Serializable]
+public class RemoteCommand 
+{
+	public String target;
+
+	public static RemoteCommand CreateFromJSON(string jsonString)
+	{
+		RemoteCommand cmd = null;
+
+		try{
+			cmd = JsonUtility.FromJson<RemoteCommand> (jsonString);
+		}
+		catch (System.Exception e ) {
+			Debug.Log (@"Something went wrong " + e);
+		}
+
+		return cmd;
+	}
+}
